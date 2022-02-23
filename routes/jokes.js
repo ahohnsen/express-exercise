737f64773d0da5f14ba8607864e0d2c2f4016e92
Andrea Hohnsen <a.hohnsen@mail.de>
2022-02-23 16:54:42 +0100

    Finish all exercises

--- routes/jokes.js
+++ routes/jokes.js
@@ -5,19 +5,19 @@ const router = express.Router();
 
 let jokes = [
   {
-    id: 0,
+    id: "0",
     joke: "Thanks for explaining the word “many” to me, it means a lot.",
   },
   {
-    id: 1,
+    id: "1",
     joke: "Why did Adele cross the road? To say hello from the other side.",
   },
   {
-    id: 2,
+    id: "2",
     joke: "What kind of concert only costs 45 cents? A 50 Cent concert featuring Nickelback.",
   },
   {
-    id: 3,
+    id: "3",
     joke: "To the person who invented zero, thanks for nothing.",
   },
 ];
@@ -27,7 +27,7 @@ let jokes = [
  * Create a GET /joke route, that returns all jokes.
  */
 router.get("/", (req, res, next) => {
-  // …
+  res.json(jokes);
 });
 
 /**
@@ -35,27 +35,56 @@ router.get("/", (req, res, next) => {
  * Create a GET /joke/:id route, that returns the joke for the given id.
  */
 
-// …
+router.get("/:id", (req, res, next) => {
+  const { id } = req.params;
+  const joke = jokes.find((joke) => joke.id === id);
+  if (joke) {
+    res.json(joke);
+  } else {
+    next();
+  }
+});
 
 /**
  * Exercise 3
  * Create a POST /joke route, that adds a new joke to the array.
  */
 
-// …
+router.post("/", (req, res, next) => {
+  const newJoke = req.body;
+  newJoke.id = nanoid();
+  jokes.push(newJoke);
+  res.json({
+    id: newJoke.id,
+  });
+});
 
 /**
  * Exercise 4
  * Create a PATCH /joke/:id route, that updates the joke for the given id.
  */
 
-// …
+router.patch("/:id", (req, res, next) => {
+  const { id } = req.params;
+  const jokeIndex = jokes.findIndex((joke) => joke.id === id);
+  jokes[jokeIndex] = {
+    ...jokes[jokeIndex],
+    ...req.body,
+    id,
+  };
+});
 
 /**
  * Exercise 5
  * Create a DELETE /joke/:id route, that deletes the joke for the given id.
  */
 
-// …
+router.delete("/:id", (req, res, next) => {
+  const { id } = req.params;
+  jokes = jokes.filter((joke) => joke.id !== id);
+  res.status(200).json({
+    deletedId: id,
+  });
+});
 
 export default router;
